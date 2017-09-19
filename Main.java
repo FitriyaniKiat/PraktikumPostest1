2f0ed83f6d64a2d31ae1f7795bdd6a60ccf7f102
FitriyaniKiat <32059949+FitriyaniKiat@users.noreply.github.com>
2017-09-20 03:22:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.HelloPostest1.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Postest1 !");
+        System.out.println("Nama Saya Fitriyani Kiat !");
+        System.out.println("Nim Saya 1500018121 !");
+  
+	}
+
+}
